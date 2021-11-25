bb78317c0290a35b37e583ee3c3ab1b4adb939b5
Vhalya <victor.gondry@ipsa.fr>
2021-11-25 03:35:33 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+const cors = require('cors');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database!'));
+
+server.use(express.json());
+server.use(cors());
+server.use(serveStatic(__dirname + '/client/dist'));
+const router = require('./characters');
+server.use('/characters', router);
+
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log("Server started!"));
